71ea586b4a319bf5e66e849554e38f5376881f07
juancastro000 <cdyutub@gmail.com>
2025-03-18 13:00:19 +0100

    first commit

---
+++ src/main/java/dev/team4/vinko/VinkoApplication.java
@@ -0,0 +1,13 @@
+package dev.team4.vinko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/team4/vinko/VinkoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.team4.vinko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
