e2e16c6ece53fce0c823100917dde006e78adba3
Emagmunioz <emagmunioz@gmail.com>
2025-03-20 15:19:01 +0100

    Create Activity entity

---
+++ src/main/java/dev/team4/vinko/entities/Activity.java
@@ -0,0 +1,56 @@
+package dev.team4.vinko.entities;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @ManyToMany(mappedBy = "activities")
+    private List<Companion> companions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Companion> getCompanions() {
+        return companions;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompanions(List<Companion> companions) {
+        this.companions = companions;
+    }
+
+    // Getters and Setters
+
+}
+
