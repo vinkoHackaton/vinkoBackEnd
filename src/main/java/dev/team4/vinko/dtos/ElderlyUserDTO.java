459c7dd5e0bb52b95695fc43325a66cb3c26eb97
monica <mosice@gmail.com>
2025-03-20 15:04:22 +0100

    feat:create entitie ElderlyUser and dto ElderlyUserDTO

---
+++ src/main/java/dev/team4/vinko/dtos/ElderlyUserDTO.java
@@ -0,0 +1,3 @@
+package dev.team4.vinko.dtos;
+
+public record ElderlyUserDTO(Long id, String name, String email, String phone) {}
--- src/main/java/dev/team4/vinko/entities/ElderlyUser.java
+++ src/main/java/dev/team4/vinko/entities/ElderlyUser.java
@@ -1,4 +1,30 @@
-package dev.team4.vinko.entities;
+package dev.team4.vinko.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
 public class ElderlyUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+    @OneToMany(mappedBy = "elderlyUser", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "elderlyUser", cascade = CascadeType.ALL)
+    private List<ContactRequest> contactRequests;
+
+    // Getters and Setters
 }
